7369b2fac2eb041844cfa1eeb2d73c4c56635f41
Arif-Mohammad-Sago <mdtalha.programmer.hero@gmail.com>
2023-11-17 12:57:17 +0600

    prettifier added

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response, Application } from "express";
-import cors from "cors";
+import express, { Request, Response, Application } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 
@@ -7,8 +7,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
 
 async function main() {
   try {
     await mongoose.connect(config.dbURL as string);
-    
+
     app.listen(config.port, () => {
       console.log(`Example app listening on Port ${config.port}`);
     });
